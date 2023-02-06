5bd1bfc081c462da234276d37499ac2e76412ced
vuongNguyenVg <vuongnguyen332003@gmail.com>
2023-02-06 17:26:14 +0700

    spring-web

---
+++ src/main/java/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+   @RequestMapping(value = "requestParam")
+   public void testRequestParam(@RequestParam String user){
+      System.out.println("User " + user);
+   }
+}
